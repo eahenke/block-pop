507f9981632724380bc2f2ef4ed1de953840a7d5
Eric Henke <eahenke@gmail.com>
2025-08-28 22:18:02 -0700

    Updates

--- sw.js
+++ sw.js
@@ -81,17 +81,17 @@ define(['./workbox-4da76df9'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon-180x180.png",
     "revision": "9d9ded1d95901de07ddd1e7d3363621c"
   }, {
-    "url": "assets/index-BkuCPZfX.js",
+    "url": "assets/index-Cc-V0joH.css",
     "revision": null
   }, {
-    "url": "assets/index-DcbRDQj4.css",
+    "url": "assets/index-tf4TN6aq.js",
     "revision": null
   }, {
     "url": "favicon.ico",
     "revision": "dc944ffdb734f0358cdb339d52ac230e"
   }, {
     "url": "index.html",
-    "revision": "58690d5a846910704b44679d2d38802e"
+    "revision": "ef3934b9efeea7f13a917d73e9d713df"
   }, {
     "url": "logo.png",
     "revision": "426acec02ed995f6b5a42cafa532e59f"
